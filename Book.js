bddfa2b2b1e839f322395ed1b5cdfb614344581e
Progameit <82383249+Progameit@users.noreply.github.com>
2021-04-12 15:26:19 +0300

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,8 @@
+function ClearFields() {
+
+     document.getElementById("name").value = "";
+     document.getElementById("famil").value = "";
+	 document.getElementById("email").value = "";
+	 document.getElementById("char").value = "";
+	 document.getElementById("Pozh").value = "";
+}
